8d72b4caaf40c5a3425a76f5a1a33368e383478f
Benedikt Schnatterbeck <benediktms@hey.com>
2023-01-12 11:58:24 +0000

    Add smooth scrolling and rainbow brackets

--- config.lua
+++ config.lua
@@ -257,10 +257,44 @@ lvim.plugins = {
 		"npxbr/glow.nvim",
 		ft = { "markdown" },
 	},
+	{
+		"karb94/neoscroll.nvim",
+		event = "WinScrolled",
+		config = function()
+			require("neoscroll").setup({
+				-- All these keys will be mapped to their corresponding default scrolling animation
+				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+				hide_cursor = true, -- Hide cursor while scrolling
+				stop_eof = true, -- Stop at <EOF> when scrolling downwards
+				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+				easing_function = nil, -- Default easing function
+				pre_hook = nil, -- Function to run before the scrolling animation starts
+				post_hook = nil, -- Function to run after the scrolling animation ends
+			})
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		event = "BufRead",
+		config = function()
+			vim.cmd("highlight default link gitblame SpecialComment")
+			vim.g.gitblame_enabled = 0
+		end,
+	},
+	{
+		"p00f/nvim-ts-rainbow",
+	},
 }
 
 lvim.builtin.treesitter.rainbow.enable = true
 lvim.builtin.treesitter.rainbow.max_file_lines = 5000
+lvim.builtin.treesitter.rainbow.colors = {
+	"Gold",
+	"Orchid",
+	"LightSkyBlue",
+}
 
 vim.api.nvim_exec(
 	[[
