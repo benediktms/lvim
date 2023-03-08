cbd873b381ec498828b3204254f91ac4a50305da
Benedikt Schnatterbeck <benediktms@hey.com>
2023-03-08 14:25:23 +0000

    Add keyboard shortcut to save in insert mode

--- config.lua
+++ config.lua
@@ -34,7 +34,8 @@ vim.opt.grepformat = "%f:%l:%c:%m"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
+lvim.keys.insert_mode["<C-s>"] = "<Esc> :w<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
