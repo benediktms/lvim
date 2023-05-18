8ad807c7c31d5757ff872533994a2cd0a6c4e1a2
Benedikt Schnatterbeck <benediktms@hey.com>
2023-05-18 09:19:10 +0100

    Add specter

--- config.lua
+++ config.lua
@@ -1,4 +1,7 @@
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+
+lvim.transparent_window = true
 lvim.format_on_save = true
 
 lvim.leader = "space"
@@ -60,6 +63,13 @@ lvim.builtin.which_key.mappings["o"] = {
 	u = { "<cmd>UndotreeToggle<cr>", "Toggle undo tree" },
 }
 
+lvim.builtin.which_key.mappings["H"] = {
+	name = "Spectre",
+	s = { "<cmd>lua require('spectre').open()<CR>", "Spectre" },
+	f = { '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>' },
+	w = { '<cmd>lua require("spectre").open_visual({select_word=true})<CR>' },
+}
+
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ command = "prettierd" },
@@ -119,7 +129,7 @@ lvim.plugins = {
 	},
 	{
 		"zbirenbaum/copilot.lua",
-		event = { "VimEnter" },
+		event = { "vimEnter" },
 		config = function()
 			vim.defer_fn(function()
 				require("copilot").setup({
@@ -241,6 +251,87 @@ lvim.plugins = {
 					},
 				},
 			})
+		end,
+	},
+	{
+		"nvim-pack/nvim-spectre",
+		config = function()
+			-- see https://github.com/nvim-pack/nvim-spectre
+			local opts = {
+				mapping = {
+					["toggle_line"] = {
+						map = "dd",
+						cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
+						desc = "toggle current item",
+					},
+					["enter_file"] = {
+						map = "<cr>",
+						cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+						desc = "goto current file",
+					},
+					["send_to_qf"] = {
+						map = "<leader>tq",
+						cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+						desc = "send all item to quickfix",
+					},
+					["replace_cmd"] = {
+						map = "<leader>c",
+						cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+						desc = "input replace vim command",
+					},
+					["show_option_menu"] = {
+						map = "<leader>o",
+						cmd = "<cmd>lua require('spectre').show_options()<CR>",
+						desc = "show option",
+					},
+					["run_current_replace"] = {
+						map = "<leader>rc",
+						cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+						desc = "replace current line",
+					},
+					["run_replace"] = {
+						map = "<leader>R",
+						cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+						desc = "replace all",
+					},
+					["change_view_mode"] = {
+						map = "<leader>v",
+						cmd = "<cmd>lua require('spectre').change_view()<CR>",
+						desc = "change result view mode",
+					},
+					["change_replace_sed"] = {
+						map = "trs",
+						cmd = "<cmd>lua require('spectre').change_engine_replace('sed')<CR>",
+						desc = "use sed to replace",
+					},
+					["change_replace_oxi"] = {
+						map = "tro",
+						cmd = "<cmd>lua require('spectre').change_engine_replace('oxi')<CR>",
+						desc = "use oxi to replace",
+					},
+					["toggle_live_update"] = {
+						map = "tu",
+						cmd = "<cmd>lua require('spectre').toggle_live_update()<CR>",
+						desc = "update change when vim write file.",
+					},
+					["toggle_ignore_case"] = {
+						map = "ti",
+						cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
+						desc = "toggle ignore case",
+					},
+					["toggle_ignore_hidden"] = {
+						map = "th",
+						cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
+						desc = "toggle search hidden",
+					},
+					["resume_last_search"] = {
+						map = "<leader>l",
+						cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
+						desc = "resume last search before close",
+					},
+				},
+			}
+			require("spectre").setup(opts)
 		end,
 	},
 }
