721439e11dbf1e2ad78f10c259f2964ec4977283
Benedikt Schnatterbeck <benediktms@hey.com>
2023-05-22 15:39:10 +0100

    Add function to toggle between camel case and snake case

--- config.lua
+++ config.lua
@@ -23,6 +23,29 @@ local function trunc(str)
 	return str
 end
 
+-- fucntion to toggle between camelCase and snake_case
+local function switch_case()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local word = vim.fn.expand("<cword>")
+	local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
+
+	-- detect camelCase
+	if word:find("[a-z][A-Z]") then
+		-- convert camelCase to snake_case
+		local snake_case_word = word:gsub("%u", "_%0"):gsub("^_", ""):lower()
+		vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
+	-- detect snake_case
+	elseif word:find("[a-z]_[a-z]") then
+		-- convert snake_case to camelCase
+		local camel_case_word = word:gsub("_(%l)", function(l)
+			return l:upper()
+		end)
+		vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { camel_case_word })
+	else
+		print("Not a snake_case or camelCase word")
+	end
+end
+
 lvim.builtin.lualine.sections.lualine_b = {
 	{
 		"branch",
@@ -66,7 +89,13 @@ lvim.builtin.which_key.vmappings["s"] = {
 lvim.builtin.which_key.mappings["o"] = {
 	name = "Other",
 	u = { "<cmd>UndotreeToggle<cr>", "Toggle undo tree" },
-	c = { "<cmd>TSContextToggle<cr>", "Toggle context" },
+	x = { "<cmd>TSContextToggle<cr>", "Toggle context" },
+	c = {
+		function()
+			switch_case()
+		end,
+		"Switch case (camelCase/snake_case)",
+	},
 }
 
 lvim.builtin.which_key.mappings["H"] = {
